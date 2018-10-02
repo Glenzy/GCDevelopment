3e393a3ef17b729241437144ddf145b54a906db3
GlenCoffey <9049346+Glenzy@users.noreply.github.com>
2018-10-03 08:21:27 +1000

    Partially Done Skill visualisation
    Using React-vis

--- src/components/Development/Content.js
+++ src/components/Development/Content.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
-import posed, { PoseGroup } from 'react-pose';
+import posed from 'react-pose';
+import {CircularGridLines, RadialChart} from 'react-vis';
 //import PropTypes from 'prop-types';
 const Image = posed.img({
     enter: {
@@ -13,31 +14,62 @@ const Image = posed.img({
       x:50
      }
 });
+
 class Content extends PureComponent {
+      state = {
+       hoveredSection: false
+     };
+      mapData = (hoveredSection) => {
+       const { skills } = this.props;
+       console.log('mapData', hoveredSection);
+       return skills.map((row, index) => {
+         return {
+           ...row,
+           innerRadius: hoveredSection === index ? row.radius + 9 : null,
+           opacity: !hoveredSection || hoveredSection === index ? 1 : 0.6
+         };
+       });
+     }
   render() {
     const { skills } = this.props;
+    const { hoveredSection } = this.state;
     return (
       <div className="content">
-        <h2>Skills that pay bills</h2>
+          <h2>Developer skillset</h2>
+        <RadialChart
+          animation
+          showLabels
+          radiusDomain={[0, 20]}
+          data={this.mapData(hoveredSection)}
+          labelsAboveChildren
+          onValueMouseOver={row => this.setState({hoveredSection: row.id})}
+          onMouseLeave={() => this.setState({hoveredSection: false})}
+          width={600}
+          height={300}
+        >
+          <CircularGridLines tickTotal={20} rRange={[0, 150]} />
+        </RadialChart>
         <div className="skillwrapper left">
+          <h4>Front-end skills</h4>
         {skills.map((skill, index) => {
           if(index < 4){
             return (
             <div key={skill.id}>
               <Image src={skill.imgSrc} className="skill-logo"/>
-              <h4 className="skill-name">{skill.level}</h4>
+              <h4 className="skill-name">{skill.radius}</h4>
               <div>'...'</div>
             </div>);
           }
         })}
       </div>
       <div className="skillwrapper right">
+        <h4>Backend skills</h4>
       {skills.map((skill, index) => {
         if(index >= 4){
           return (
           <div key={skill.id}>
               <Image src={skill.imgSrc} className="skill-logo" />
-            <h4 className="skill-name">{skill.level}</h4>
+            <h4 className="skill-name">{skill.radius}</h4>
             <div>'...'</div>
           </div>);
         }
